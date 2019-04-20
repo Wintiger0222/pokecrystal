cfd284325bbee1013f4b088d45490e3ba28ac87f
6B6390AA <49777427+6B6390AA@users.noreply.github.com>
2019-04-20 23:01:38 +0900

    why
    plz kill meh

--- data/phone/text/beth_overworld.asm
+++ data/phone/text/beth_overworld.asm
@@ -1,56 +1,56 @@
 BethAskNumber1Text:
-	text "It's been a while"				;? ? ?? ???
-	line "since I lost."					;??? ?? ???
+	text "It's been a while"				;그 때 졌던 이후로
+	line "since I lost…"					;시간이 많이 지났네…
 
-	para "You're very good"					;? ???? ? ????
+	para "You're very good"					;너는 포켓몬을 잘 다루니까
 	line "at battling."
 
-	para "We should battle"					;?? ??? ??? ??
-	line "again sometime."					;?? ? ??
+	para "We should battle"					;다시 배틀을 해보는 것도
+	line "again sometime."					;좋을 거 같아
 
-	para "By challenging a"					;??? ?? ??????
-	line "tough trainer like"				;??? ?? ??
-
-	para "you, I think I can"				;?? ??? ? ?? ??
+	para "By challenging a"					;너처럼 강한 트레이너들과
+	line "tough trainer like"				;배틀을 하게 되면
+	
+	para "you, I think I can"				;나도 강해질 수 있을 거야
 	line "get better too."
 
-	para "So how about it?"					;???
+	para "So how about it?"					;그래서
 
-	para "Would you give me"				;??? ?? ???
-	line "your phone number?"				;??? ? ????
+	para "Would you give me"				;번호를 알고 싶은데
+	line "your phone number?"				;알려줄 수 있을까?
 	done
 
 BethAskNumber2Text:
-	text "By challenging a"					;??? ?? ??????
-	line "tough trainer like"				;??? ?? ??
-
-	para "you, I think I can"				;?? ??? ? ?? ??
+	text "By challenging a"					;너처럼 강한 트레이너들과
+	line "tough trainer like"				;배틀을 하게 되면
+	
+	para "you, I think I can"				;나도 강해질 수 있을 거야
 	line "get better too."
 
-	para "So how about it?"					;???
+	para "So how about it?"					;그래서
 
-	para "Would you give me"				;??? ?? ???
-	line "your phone number?"				;??? ? ????
+	para "Would you give me"				;번호를 알고 싶은데
+	line "your phone number?"				;알려줄 수 있을까?
 	done
 
 BethNumberAcceptedText:
-	text "Let's battle again"				;??? ?? ?? ???!
+	text "Let's battle again"				;나중에 다시 배틀 해보자!
 	line "sometime!"
 	done
 
 BethNumberDeclinedText:
-	text "That's too bad."				;??? ?? ? ??
+	text "That's too bad…"				;그럼 하는 수 없네…
 
-	para "Let me know if you"				;??? ???
-	line "change your mind."				;?? ???
+	para "Let me know if you"				;마음이 바뀌면
+	line "change your mind."				;다시 들러줘
 	done
 
 BethPhoneFullText:
-	text "Oh no. Your phone"				;?????
-	line "is out of memory."				;? ? ? ???
+	text "Oh no. Your phone"				;전화번호가
+	line "is out of memory."				;꽉 찬 거 같은데
 	done
 
 BethRematchText:
-	text "You sure kept me"					;?? ?????
-	line "waiting! Let's go!"				;?? ?????!
+	text "You sure kept me"					;오래 기다렸다구
+	line "waiting! Let's go!"				;그럼 시작해볼까!
 	done
