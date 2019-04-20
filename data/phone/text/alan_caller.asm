e564d54db80e13f59ca03d8e6428d38dfbd7b9cb
6B6390AA <49777427+6B6390AA@users.noreply.github.com>
2019-04-20 22:48:20 +0900

    fixed some encoding errors
    at least i think this did

--- data/phone/text/alan_caller.asm
+++ data/phone/text/alan_caller.asm
@@ -1,98 +1,98 @@
 AlanGettingStrongerText:
-	text "My @"								;???? ? ????
+	text "My @"								;내 @
 	text_ram wStringBuffer4
-	text "'s"								;?? ???? ??!
-	line "getting stronger,"
+	text "'s"								;계산대로
+	line "getting stronger,"				;점점 강해지고 있어
 
 	para "exactly as I"
 	line "calculated!"
 	done
 
 UnknownText_0x64cf3:
-	text "By the way, we"					;??? ??
-	line "knocked out a wild"				;???
+	text "By the way, we"					;그리고 어제
+	line "knocked out a wild"				;야생의
 
 	para "@"								;@
 	text_ram wStringBuffer4
-	text " just"							;??? ???? ?? ???
+	text " just"							;하나를 쓰러뜨린 적 있지
 	line "the other day."
 
-	para "Studying up in"					;?????? ??!
+	para "Studying up in"					;유비무환이란 거네!
 	line "advance worked!"
 	done
 
 UnknownText_0x64d4f:
-	text "By the way, a wild"				;???? ??
-	line "@"										;??? @
+	text "By the way, a wild"				;그나저나 어제
+	line "@"										;야생의 @
 	text_ram wStringBuffer4
-	text " escaped"							;? ?? ????
-	cont "on me yesterday."					;??? ???
+	text " escaped"							;한 마리가 눈 앞에서
+	cont "on me yesterday."					;도망쳐 버렸어
 
-	para "A computational"					;??? ??? ??? ?
+	para "A computational"					;계산에 실수가 있었나 봐
 	line "error on my party."
 	done
 
 UnknownText_0x64da4:
-	text "I've studied quite"				;? ??? ??? ??
+	text "I've studied quite"				;그 때부터 공부를 해서
 	line "a bit since then,"
 
-	para "and I've gotten a"				;??? ????!
+	para "and I've gotten a"				;실력이 좋아졌어
 	line "lot better!"
 
-	para "I'm hanging out on"			;?? ??
+	para "I'm hanging out on"			;지금 나는
 	line "@"								;@
 	text_ram wStringBuffer5
-	text "."								;??
+	text "."								;이야
 
-	para "Can you come down"				;?? ? ???
+	para "Can you come down"				;승부 안 해볼래?
 	line "for a battle?"
 	done
 
 UnknownText_0x64e1f:
-	text "See you later!"					;? ?!
+	text "See you later!"					;그럼 또 봐!
 	done
 
 UnknownText_0x64e2f:
-	text "Hehehe, I picked"					;?? ?? ? ?????!
+	text "Hehehe, I picked"					;헤헤 좋은 걸 발견했다구!
 	line "up something nice!"
 
-	para "You can have it!"					;??? ???
+	para "You can have it!"					;가져가려면 와도 돼
 	line "Why don't you come"
 
 	para "to @"									;@
 	text_ram wStringBuffer5
 	text_start
-	line "and pick it up?"						;? ???!
+	line "and pick it up?"						;에 오면 바로 줄게!
 	done
 
 UnknownText_0x64e90:
-	text "I haven't picked"					;?? ?? ??
-	line "up anything yet."					;? ???
+	text "I haven't picked"					;아직 딱히 특별한 건
+	line "up anything yet."					;못 봤네
 
-	para "I'll call you if I"				;?? ???
-	line "find something."					;??? ????
+	para "I'll call you if I"				;하나 찾으면
+	line "find something."					;곧바로 연락할게
 	done
 
 UnknownText_0x64ed4:
-	text "If we don't battle"				;?? ??? ? ???
-	line "soon, I'll forget"				;??? ???? ? ??
+	text "If we don't battle"				;슬슬 승부를 안 한다면
+	line "soon, I'll forget"				;전략을 전부 까먹을 거 같아
 	cont "my strategy!"
 
-	para "I'm waiting on"					;@
-	line "@"										;?? ???? ???!
+	para "I'm waiting on"					;그럼 난 @
+	line "@"										
 	text_ram wStringBuffer5
-	text "!"
+	text "!"								;에서 기다릴게
 	done
 
 UnknownText_0x64f1a:
-	text "I have to do my"					;???? ???
-	line "homework, so can"					;?? ?? ????
+	text "I have to do my"					;숙제해야 되니까
+	line "homework, so can"					;빨리 와 줄래?
 
 	para "you come get your"
 	line "gift right away?"
 
-	para "I'm waiting on"					;? @
-	line "@"								;? ????
+	para "I'm waiting on"					;난 @
+	line "@"							
 	text_ram wStringBuffer5
-	text "!"
+	text "!"								;에 있으니까
 	done
