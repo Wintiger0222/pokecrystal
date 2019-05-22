f293b1d6065f7d4aa17f89c4e5c6f61a91d33124
6B6390AA <49777427+6B6390AA@users.noreply.github.com>
2019-05-22 22:31:48 +0900

    asdf
    asdf

--- data/phone/text/brent_caller.asm
+++ data/phone/text/brent_caller.asm
@@ -1,62 +1,64 @@
 BrentRareTradeText:
-	text "Oh yeah, I got an"
-	line "extremely rare"
+	text "Oh yeah, I got an"				;그러고 보니 전에
+	line "extremely rare"					;엄청 희귀한 포켓몬을
 
-	para "#MON in a trade"
-	line "a while back."
+	para "#MON in a trade"					;교환하다가
+	line "a while back."					;손에 넣었어
 
-	para "Do you want to"
-	line "know what it is?"
+	para "Do you want to"					;무슨 포켓몬인지
+	line "know what it is?"					;알려줄까나?
 
-	para "Hehe, I'm keeping"
-	line "it a secret!"
+	para "Hehe, I'm keeping"				;흐흐흐…
+	line "it a secret!"						;역시 비밀로 해야지
 	done
 
 UnknownText_0x6613c:
-	text "Oh yeah, I took"
+	text "Oh yeah, I took"					;요전에 야생의
 	line "down this wild"
 
-	para "@"
+	para "@"								;@
 	text_ram wStringBuffer4
-	text "."
-	line "It wasn't rare"
-
-	para "enough to bother"
-	line "catching."
+	text "."								;하고 맞닥뜨렸어
+	
+	
+	para "It wasn't rare"					;굳이 잡을 정도로 귀한
+	line "enough to bother"					;포켓몬은 아니었지만
+	line "catching."						;아 맞다 그리고…
 	done
 
 UnknownText_0x6618c:
-	text "Oh yeah, I saw a"
-	line "rare #MON about"
+	text "Oh yeah, I saw a"					;1시간 쯤 전에
+	line "rare #MON about"					;희귀한 포켓몬을 봤어
 	cont "an hour ago."
 
-	para "It was my first"
-	line "sighting. But I"
+	para "It was my first"					;처음 보는 녀석이었는데
+	line "sighting. But I"					;몬스터볼이 없어서
 
-	para "didn't have any"
+	para "didn't have any"					;잡지 못하게 됐어…
 	line "# BALLS…"
 
-	para "Would BILL laugh"
-	line "at my mistakes?"
+	para "Would BILL laugh"					;이런 자세론 이수재님께
+	line "at my mistakes?"					;웃음거리가 될까?
 	done
 
 UnknownText_0x66214:
-	text "You've got time"
+	text "You've got time"					;여전히 한가해 보이네
 	line "like usual, right?"
 
-	para "Feel like having a"
-	line "battle?"
+	para "Feel like having a"				;포켓몬 승부라도
+	line "battle?"							;하지 않겠어?
 
-	para "It'll be a chance"
-	line "to see my rare"
+	para "It'll be a chance"				;내 귀한 포켓몬도
+	line "to see my rare"					;볼 수 있고
 
-	para "#MON. It'll be"
-	line "worth your time!"
+	para "#MON. It'll be"				;가치 있는 승부가
+	line "worth your time!"					;될 테니까
 
-	para "You know where--"
-	line "@"
+	para "I'm always at"
+	line "@"								;@
 	text_ram wStringBuffer5
-	text "."
+	text "."								;에 있으니까
+	text "So feel free to visit!"			;내키면 들러줘!
 	done
 
 UnknownText_0x662a9:
