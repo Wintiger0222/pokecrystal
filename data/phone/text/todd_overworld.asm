2e3acc764f111d72684413fc2d09f0157f36080d
6B6390AA <49777427+6B6390AA@users.noreply.github.com>
2019-06-24 19:40:04 +0900

    Update todd_overworld.asm

--- data/phone/text/todd_overworld.asm
+++ data/phone/text/todd_overworld.asm
@@ -1,68 +1,69 @@
 ToddAskNumber1Text:
-	text "From here, I can"
-	line "see GOLDENROD's"
+	text "From here, I can"					;여기서 금빛시티
+	line "see GOLDENROD's"					;백화점을 보니까
 
-	para "DEPT.STORE. They"
-	line "have bargain sales"
+	para "DEPT.STORE. They"					;때때로 옥상에서
+	line "have bargain sales"				;할인을 하는 모양인데
 
-	para "up on the rooftop"
+	para "up on the rooftop"				
 	line "every so often."
 
-	para "Could I get your"
-	line "phone number?"
+	para "Could I get your"					;이번에 할인을 하면
+	line "phone number?"					;연락할 테니까
 
-	para "I'll call you when"
+	para "I'll call you when"				;전화번호 받아도 돼?
 	line "they have a sale."
 	done
 
 ToddAskNumber2Text:
-	text "If you give me"
-	line "your number, I'll"
+	text "If you give me"					;이번에 할인을 하면
+	line "your number, I'll"				;연락할 테니까
 
-	para "call you when they"
+	para "call you when they"				;전화번호 받아도 돼?
 	line "have a sale."
 	done
 
 ToddNumberAcceptedText:
-	text "I check GOLDENROD"
-	line "DEPT.STORE every"
+	text "I check GOLDENROD"				;매일 보고 있으니까
+	line "DEPT.STORE every"					;할인하고 있을 땐
 
-	para "day, so I know"
+	para "day, so I know"					;바로 알 수 있어!
 	line "when there's a"
 	cont "sale."
 	done
 
 ToddNumberDeclinedText:
-	text "Anyway…"
+	text "Anyway…"							;뭐 어쩔 수 없지만…
 
-	para "If you're short on"
-	line "money, take down"
-	cont "my number."
+	para "If you're short on"				;주머니 사정이 나쁘면
+	line "money, take down"					;전화번호 교환하러
+	cont "my number."						;한 번 들러줘!
 
 	para "I'll call you when"
 	line "there's a sale."
 	done
 
 ToddPhoneFullText:
-	text "Your phone doesn't"
-	line "have enough memory"
-	cont "for more numbers."
+	text "Your phone doesn't"				;…가 아니라
+	line "have enough memory"				;번호가 꽉 차서
+	cont "for more numbers."				;등록이 안 되는데?
 	done
 
 ToddRematchText:
-	text "I waited for you!"
-	line "I even cut back my"
+	text "I waited for you!"				;기다렸다고!
+	line "I even cut back my"				;물건을 살 돈으로
 
-	para "shopping to raise"
-	line "my #MON better!"
+	para "shopping to raise"				;이 날만을 위해서
+	line "my #MON better!"					;포켓몬을 키워왔지!
 	done
+
 
 UnusedText_0xa43ca:
 ; This is unused; Todd says this during the department store sale,
 ; but this is a duplicate of the actual string in the Route 34 scripts.
-	text "Shopping under the"
+	text "Shopping under the"				;하늘 아래에서 쇼핑!
 	line "sky!"
 
-	para "It feels so nice"
-	line "up on a rooftop."
+	para "It feels so nice"					;옥상은 역시
+	line "up on a rooftop."					;탁 트여서 좋네
 	done
